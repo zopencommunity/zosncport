4cf31bdd7efd37a2efe013fdc8844c09cec2eb28
Igor Todorovski <itodorov@ca.ibm.com>
2023-11-14 16:30:41 -0500

    Fix buildenv and groovy

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -9,6 +9,6 @@ node('linux')
       userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/zosncport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/zosncport.git'), string(name: 'PORT_DESCRIPTION', value: 'Simplified nc (netcat), tcp only. Connect stdin, stdout to an open tcp connection's output/input' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/zosncport.git'), string(name: 'PORT_DESCRIPTION', value: 'Simplified nc (netcat), tcp only. Connect stdin, stdout to an open tcp connections output/input' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
   }
 }
