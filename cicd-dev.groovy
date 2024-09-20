dd8295a2185f3c4e332b7914901c097924647e80
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:37:09 -0400

    Change organization to zopencommunity

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -13,9 +13,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/zosncport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/zosncport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/zosncport.git'), string(name: 'PORT_DESCRIPTION', value: 'Simplified nc (netcat), tcp only. Connect stdin, stdout to an open tcp connections output/input' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/zosncport.git'), string(name: 'PORT_DESCRIPTION', value: 'Simplified nc (netcat), tcp only. Connect stdin, stdout to an open tcp connections output/input' ), string(name: 'BUILD_LINE', value: 'DEV') ]
   }
 }
